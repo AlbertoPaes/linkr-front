b600ca52f47da834275cd6193d2fd660280b3f7f
Alberto <tejotaesi@gmail.com>
2022-06-19 12:32:38 -0300

    feat: private /users/:id route

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import urlMetadata from "url-metadata";
 export const api = axios.create({
   /*Fix me: Ao criar uma nova instância do axios, a baseURL não está aceitando a variável de ambiente. */
   // baseURL: `${process.env.REACT_APP_API_BASE_URL}`,
-  //baseURL: "https://linkr-driven-api.herokuapp.com",
-  baseURL: "http://localhost:4000"
+  baseURL: "https://linkr-driven-api.herokuapp.com",
+  //baseURL: "http://localhost:4000"
 });
 
 export const makeSignUp = async (formData) => {
