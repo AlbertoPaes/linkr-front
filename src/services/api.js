2640378555aa247772ce0bc33d36c2d428dac9c5
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-06-24 17:42:39 -0300

    fix: heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import urlMetadata from "url-metadata";
 
 export const api = axios.create({
-  // baseURL: "https://linkr-driven-api.herokuapp.com"
-  baseURL: "http://localhost:4000"
+  baseURL: "https://linkr-driven-api.herokuapp.com"
 });
 
 export const makeSignUp = async (formData) => {
